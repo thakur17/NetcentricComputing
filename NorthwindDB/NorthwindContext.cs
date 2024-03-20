a7b15fc120ce1e5521d78544469e5db67b335c56
thakur17 <42466300+thakur17@users.noreply.github.com>
2024-03-20 06:05:57 +0545

    Add files via upload

---
+++ NorthwindDB/NorthwindContext.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace NorthwindDb;
+
+public partial class NorthwindContext : DbContext
+{
+    public NorthwindContext()
+    {
+    }
+
+    public NorthwindContext(DbContextOptions<NorthwindContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+     public virtual DbSet<Product> Products { get; set; }
+
+    
+    public virtual DbSet<Supplier> Suppliers { get; set; }
+
+    
+   
+    }
+
+    
---
+++ NorthwindDB/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NorthwindDb
+    {
+        public class Product
+        {
+            public int ProductId { get; set; } // The primary key.
+            [Required]
+            [StringLength(40)]
+            public string ProductName { get; set; } = null!;
+            // Property name is different from the column name.
+            [Column("UnitPrice", TypeName = "money")]
+            public decimal? Cost { get; set; }
+            [Column("UnitsInStock")]
+            public short? Stock { get; set; }
+            public bool Discontinued { get; set; }
+            // These two properties define the foreign key relationship
+            // to the Categories table.
+            public int CategoryId { get; set; }
+            public virtual Category Category { get; set; } = null!;
+        }
+    }
+
+
