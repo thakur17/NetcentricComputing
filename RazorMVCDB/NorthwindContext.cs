902ec542d08420d43519e22ee67a037e9346f851
thakur17 <42466300+thakur17@users.noreply.github.com>
2025-07-24 08:17:59 +0545

    Add files via upload

---
+++ RazorMVCDB/NorthwindContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace RazorWithDB.NorthwindDB
+{
+    public class NorthwindContext : DbContext
+    {
+
+        public NorthwindContext()
+        {
+                
+        }
+        public NorthwindContext(DbContextOptions<NorthwindContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Supplier> Suppliers { get; set; }
+
+
+
+    }
+
+
+}
---
+++ RazorMVCDB/Supplier.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorWithDB.NorthwindDB
+{
+     public class Supplier
+        {
+            [Key]
+            public int SupplierId { get; set; }
+
+            [Column(TypeName = "nvarchar (40)")]
+            [StringLength(40)]
+            [Required]
+            public string CompanyName { get; set; } = null!;
+
+            [Column(TypeName = "nvarchar (30)")]
+            [StringLength(30)]
+            public string? Country { get; set; }
+
+            [Column(TypeName = "nvarchar (24)")]
+            [StringLength(24)]
+            public string? Phone { get; set; }
+
+         
+        }
+    }
+
