e3868d10ffdc2c0600dd0d70bb2666c0acf49f03
thakur17 <42466300+thakur17@users.noreply.github.com>
2025-08-15 06:44:43 +0545

    Add files via upload

--- CRUDMVC/HomeController.cs
+++ CRUDMVC/HomeController.cs
@@ -1,31 +1,50 @@
-using Microsoft.AspNetCore.Mvc;
-using mvcProj.Models;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SessionCookieDemo.Models;
 
-namespace mvcProj.Controllers
+namespace SessionCookieDemo.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+       
+        
         public IActionResult Index()
         {
+            CookieOptions options = new CookieOptions();
+            options.Expires = DateTime.Now.AddDays(1);
+            options.IsEssential = true;
+            options.Path = "/";
+
+            HttpContext.Response.Cookies.Append("MyCookie", "Testing for cookie", options);
+
+
+            HttpContext.Session.SetString("SessionKey", "Thakur");
             return View();
         }
         public IActionResult AboutUs()
         {
+            ViewBag.Data=HttpContext.Session.GetString("SessionKey");
             return View();
         }
-
         public IActionResult Privacy()
         {
+            ViewBag.Data = HttpContext.Session.GetString("SessionKey");
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            if (HttpContext.Session.GetString("SessionKey") != null)
+            {
+                HttpContext.Session.Remove("SessionKey");
+
+            }
             return View();
         }
 
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
