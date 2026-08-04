ef559d51c05adfc518ed7a379d7ad813183a9fdc
thakur17 <42466300+thakur17@users.noreply.github.com>
2026-08-04 01:36:10 -0700

    Add files via upload

---
+++ RazorMVCDB/SupplierContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace wesqlite.Model
+{
+    public class SupplierContext:DbContext
+    {
+        public SupplierContext()
+        {
+
+        }
+        public SupplierContext(DbContextOptions<SupplierContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Supplier> Suppliers { get; set; }
+
+    }
+}
