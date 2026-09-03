499da3dddb86c280eadd095fa7914dd2f55d5b54
thakur17 <42466300+thakur17@users.noreply.github.com>
2026-09-02 18:48:46 -0700

    Add files via upload

---
+++ WebAPI/Program.cs
@@ -0,0 +1,42 @@
+
+using Microsoft.EntityFrameworkCore;
+using Webapplication1.Models;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<SupplierContext>
+     (options => options.UseSqlite(builder.Configuration.GetConnectionString("Connection")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
