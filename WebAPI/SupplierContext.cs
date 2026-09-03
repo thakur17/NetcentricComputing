6d35dffee378f314b88fcdbe24c8eb5b76de7f6e
thakur17 <42466300+thakur17@users.noreply.github.com>
2026-09-02 18:49:56 -0700

    Add files via upload

---
+++ WebAPI/SupplierContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Webapplication1.Models
+{
+    public class SupplierContext : DbContext
+    {
+
+        public SupplierContext()
+        {
+
+        }
+        public SupplierContext(DbContextOptions<SupplierContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Supplier> Suppliers { get; set; }
+
+
+
+    }
+}
